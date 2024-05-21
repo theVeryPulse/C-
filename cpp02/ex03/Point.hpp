a6870b135f8c18dfa719cbebc3f410ae8899ab6a
Philip <macpilot23@outlook.com>
2024-05-21 22:57:26 +0100

    2024.05.21

---
+++ cpp02/ex03/Point.hpp
@@ -0,0 +1,24 @@
+class Point
+{
+  public:
+    //=============== Types: Classes, Enums, and Aliases (using) ===============
+    //================= Constructors, Assignments, Destructor ==================
+    Point();
+    ~Point();
+    Point(const Point& other); // Copy constructor
+    Point& operator=(const Point& other); // Copy assignment operator
+    //=============================== Functions ================================
+    //================================== Data ==================================
+
+  protected:
+    //=============== Types: Classes, Enums, and Aliases (using) ===============
+    //================= Constructors, Assignments, Destructor ==================
+    //=============================== Functions ================================
+    //================================== Data ==================================
+
+  private:
+    //=============== Types: Classes, Enums, and Aliases (using) ===============
+    //================= Constructors, Assignments, Destructor ==================
+    //=============================== Functions ================================
+    //================================== Data ==================================
+};
