63605451df4edd8e440e92fceadce1535755d1a9
Philip <macpilot23@outlook.com>
2024-08-14 17:43:06 +0100

    refactor (cpp06ex01): cast uintptr_t directly rather than its reference
    rename executable, add one more test case

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,10 +6,14 @@
 int main(void)
 {
     Data      data;
+    data.char_value = '!';
     uintptr_t a = Serializer::serialize(&data);
     if (Serializer::deserialize(a) == &data)
         std::cout << "same\n";
     else
         std::cout << "not same\n";
+    Data& same = *Serializer::deserialize(a);
+    std::cout << same.char_value << "\n";
+
     return 0;
 }
