444210d9afc69230e912d6fd242dcebf87d8946d
Philip <macpilot23@outlook.com>
2024-08-16 18:51:10 +0100

    test (cpp07ex00): string test

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -10,5 +10,18 @@ int main()
     std::cout << "swapped: " << a << " " << b << "\n";
     std::cout << "min: " << min(a, b) << "\n";
     std::cout << "max: " << max(a, b) << "\n";
+
+    a = 2;
+    b = 3;
+    swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    std::cout << "min( a, b ) = " << min( a, b ) << "\n";
+    std::cout << "max( a, b ) = " << max( a, b ) << "\n";
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << "\n";
+    std::cout << "min( c, d ) = " << ::min( c, d ) << "\n";
+    std::cout << "max( c, d ) = " << ::max( c, d ) << "\n";
     return 0;
 }
