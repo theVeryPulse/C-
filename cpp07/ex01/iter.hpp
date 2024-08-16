198a8006ebb5d35557263ad1bc5f7f48d3cbdae9
Philip <macpilot23@outlook.com>
2024-08-16 20:45:05 +0100

    refactor (cpp07ex01): use index rather than pointer to loop through

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -7,8 +7,8 @@
 
 template <typename T> void iter(T array[], size_t length, void(*f)(T& element))
 {
-    for (T* element = array; element < array + length; ++element)
-        f(*element);
+    for (size_t i = 0; i < length; ++i)
+        f(array[i]);
 }
 
 template <typename T> void print(T value)
